4ef2b7f4f113cf47db6e143871626703995c696d
Guga-Nascimento <guganascimento001@gmail.com>
2023-08-31 15:35:07 -0300

    Hotflix precision e scale

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -26,5 +26,38 @@ public class User {
 	private List<Feature> features;
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<News> news;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	public Card getCard() {
+		return card;
+	}
+	public void setCard(Card card) {
+		this.card = card;
+	}
+	public List<Feature> getFeatures() {
+		return features;
+	}
 
+	public List<News> getNews() {
+		return news;
+	}
+
+
+	
 }
